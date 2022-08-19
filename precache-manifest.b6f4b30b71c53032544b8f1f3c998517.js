08e8c84ca515d785f9d6a5be7f340c6f1090ecd0
diego_verticis <diego@verticis.com.br>
2022-08-19 10:46:48 -0300

    Updates

---
+++ precache-manifest.b6f4b30b71c53032544b8f1f3c998517.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cedf7631e55a27e45b2a",
+    "url": "/react-portfolio/static/css/main.f060b0f2.chunk.css"
+  },
+  {
+    "revision": "cedf7631e55a27e45b2a",
+    "url": "/react-portfolio/static/js/main.cedf7631.chunk.js"
+  },
+  {
+    "revision": "4c548e320c14bf8c5e09",
+    "url": "/react-portfolio/static/js/1.4c548e32.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "1904247ccce0e6cbdb747066c83209d3",
+    "url": "/react-portfolio/static/media/me-2.1904247c.png"
+  },
+  {
+    "revision": "ab9803b503070a4529cd99f7e12b8ad8",
+    "url": "/react-portfolio/static/media/cv_br.ab9803b5.pdf"
+  },
+  {
+    "revision": "e642a80fa40cfe322dd60d5a9aefc4a0",
+    "url": "/react-portfolio/static/media/me.e642a80f.png"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/react-portfolio/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/react-portfolio/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/react-portfolio/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "146a57aa82c60b620f30eb2d1d8e9477",
+    "url": "/react-portfolio/index.html"
+  }
+];
